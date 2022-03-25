54b1149a6792bc8d38a05f6d16d764d100ed529e
Shiloli-r <ronaldshiloli@gmail.com>
2022-03-25 10:27:31 +0300

    Register button

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-      MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(),
-    )
-  );
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: HomePage(),
+  ));
 }
 
 class HomePage extends StatefulWidget {
@@ -23,7 +21,14 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Register")),
+      appBar: AppBar(
+        title: const Text("Register"),
+      ),
+      body: Center(
+          child: TextButton(
+              onPressed: () async {
+                
+              }, child: const Text("Register"))),
     );
   }
 }
