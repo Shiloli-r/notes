9a68f4cda80f95b2e250702c9d6b7c4938703d5a
Shiloli-r <ronaldshiloli@gmail.com>
2022-03-26 11:38:39 +0300

    Registration textfields and editing controllers

--- lib/main.dart
+++ lib/main.dart
@@ -6,29 +6,59 @@ void main() {
     theme: ThemeData(
       primarySwatch: Colors.blue,
     ),
-    home: HomePage(),
+    home: const HomePage(),
   ));
 }
 
 class HomePage extends StatefulWidget {
-  HomePage({Key? key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  late final TextEditingController _emailController;
+  late final TextEditingController _passwordController;
+
+  @override
+  void initState() {
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Register"),
       ),
-      body: Center(
-          child: TextButton(
-              onPressed: () async {
-                
-              }, child: const Text("Register"))),
+      body: Column(
+        children: [
+          TextField(
+            controller: _emailController,
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(hintText: "Email")
+          ),
+             TextField(
+            controller: _passwordController,
+            obscureText: true,
+            autocorrect: false,
+            enableSuggestions: false,
+            decoration: const InputDecoration(hintText: "Password")
+          ),
+          TextButton(onPressed: () async {}, child: const Text("Register")),
+        ],
+      ),
     );
   }
 }
