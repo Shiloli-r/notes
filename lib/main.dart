642e8d085d7cb140bdbd67e0bc7e427fce793ffd
Shiloli-r <ronaldshiloli@gmail.com>
2022-03-29 07:30:19 +0300

    VerifyEmailView

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:notes/views/login_view.dart';
 
 import 'firebase_options.dart';
 
@@ -32,15 +31,37 @@ class HomePage extends StatelessWidget {
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-              if (FirebaseAuth.instance.currentUser?.emailVerified ?? false) {
+              final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false) {
                 return const Text("Email Verified");
               } else {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const VerifyEmailView(),
+                ));
                 return const Text("You need to verify your Email");
               }
             default:
               return const Text("Loading...");
           }
         },
+      ),
+    );
+  }
+}
+
+class VerifyEmailView extends StatefulWidget {
+  const VerifyEmailView({Key? key}) : super(key: key);
+
+  @override
+  State<VerifyEmailView> createState() => _VerifyEmailViewState();
+}
+
+class _VerifyEmailViewState extends State<VerifyEmailView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Verify Email"),
       ),
     );
   }
