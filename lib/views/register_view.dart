c91dbb66d3d748c0ae133f3ccd33c7204d789aa9
Shiloli-r <ronaldshiloli@gmail.com>
2022-03-30 08:58:17 +0300

    fixed push error

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,7 +1,5 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:notes/firebase_options.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
@@ -30,53 +28,34 @@ class _RegisterViewState extends State<RegisterView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Register"),
-      ),
-      body: FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              return Column(
-                children: [
-                  TextField(
-                      controller: _emailController,
-                      autocorrect: false,
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: const InputDecoration(hintText: "Email")),
-                  TextField(
-                      controller: _passwordController,
-                      obscureText: true,
-                      autocorrect: false,
-                      enableSuggestions: false,
-                      decoration: const InputDecoration(hintText: "Password")),
-                  TextButton(
-                      onPressed: () async {
-                        final email = _emailController.text;
-                        final password = _passwordController.text;
-                        try {
-                          await FirebaseAuth.instance
-                            .createUserWithEmailAndPassword(
-                                email: email, password: password);
-                        } on FirebaseAuthException catch (e) {
-                          if(e.code == 'weak-password'){}
-                          else if(e.code == 'email-already-in-use'){}
-                          else if(e.code == 'invalid-email'){}
-                        }
-                        
-                      },
-                      child: const Text("Register")),
-                ],
-              );
-            default:
-              return const Text("Loading...");
-          }
-        },
-      ),
+    return Column(
+      children: [
+        TextField(
+            controller: _emailController,
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(hintText: "Email")),
+        TextField(
+            controller: _passwordController,
+            obscureText: true,
+            autocorrect: false,
+            enableSuggestions: false,
+            decoration: const InputDecoration(hintText: "Password")),
+        TextButton(
+            onPressed: () async {
+              final email = _emailController.text;
+              final password = _passwordController.text;
+              try {
+                await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                    email: email, password: password);
+              } on FirebaseAuthException catch (e) {
+                if (e.code == 'weak-password') {
+                } else if (e.code == 'email-already-in-use') {
+                } else if (e.code == 'invalid-email') {}
+              }
+            },
+            child: const Text("Register")),
+      ],
     );
   }
 }
